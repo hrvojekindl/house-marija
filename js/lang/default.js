2f0b084a673414bd803d108310c92b62c6d9b670
Hrvoje Kindl <hrvoje.kindl@gmail.com>
2013-02-17 16:03:45 +0100

    markers

--- js/lang/default.js
+++ js/lang/default.js
@@ -1,3 +1,7 @@
 window.lang = {
-	house_marija: 'House Marija'
+	house_marija: 'House Marija',
+	saharun_beach: 'Saharun beach',
+	veli_rat_lighthouse: 'Veli Rat lighthouse',
+	telascica_nature_park: 'Telašćica Nature Park',
+	kornati_national_park: 'Kornati National Park'
 };

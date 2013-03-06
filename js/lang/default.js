8cefa8b88f171b70fa51c8d9f3476df133ac31f8
Hrvoje Kindl <hrvoje.kindl@gmail.com>
2013-03-06 19:49:03 +0100

    images sort

--- js/lang/default.js
+++ js/lang/default.js
@@ -1,10 +1,15 @@
 window.lang = {
+	// markers
 	house_marija: 'House Marija',
 	saharun_beach: 'Saharun beach',
 	veli_rat_lighthouse: 'Veli Rat lighthouse',
 	telascica_nature_park: 'Telašćica Nature Park',
 	kornati_national_park: 'Kornati National Park',
+	
+	// lightbox
 	lightbox_label_image: 'Image',
 	lightbox_label_of: 'of',
-	house_title_1: 'Naslov1'
+	
+	// image titles
+	about_title_1: 'View from the terrace'
 };
